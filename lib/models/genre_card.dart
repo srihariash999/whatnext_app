52c1d6f602dc4f19e5256cb0e542f46cb519f63b
RajaVamsi11 <rajavamsi1998@gmail.com>
2021-03-10 12:04:40 +0530

    UI chnages in details page

---
+++ lib/models/genre_card.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:whatnext/ui/shared/shared_styles.dart';
+
+class GenreCard extends StatelessWidget {
+  final String genre;
+
+  const GenreCard({Key key, this.genre}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      margin: EdgeInsets.only(left: kDefaultPadding),
+      padding: EdgeInsets.symmetric(
+        horizontal: kDefaultPadding,
+        vertical: kDefaultPadding / 4, // 5 padding top and bottom
+      ),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.blueGrey),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Text(
+        genre,
+        style: TextStyle(color: kTextColor.withOpacity(0.8), fontSize: 16),
+      ),
+    );
+  }
+}
--- lib/ui/shared/app_colors.dart
+++ lib/ui/shared/app_colors.dart
@@ -4,3 +4,4 @@ const Color lightGrey = Color.fromARGB(255,61,63,69);
 const Color darkGrey = Color.fromARGB(255,18,18,19);
 const Color primaryColor =  Color.fromARGB(255, 9, 202, 172);
 const Color backgroundColor = Color.fromARGB(255, 26, 27, 30);
+const kTextLightColor = Color(0xFF9A9BB2);
